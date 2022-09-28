86e51da27283b5b574fa6cb57cef3ac998e757e2
Nacho <nacho_085@hotmail.com>
2022-09-28 17:58:28 +0200

    Guardado de seguridad

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Todo
+
 class Category extends Model
 {
     use HasFactory;
+
+    public function todos(){
+        return $this->hasMany(Todo::class); // hasMany Indica que una categoría puede tener muchos todos
+    }
 }
