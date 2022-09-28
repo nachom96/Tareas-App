dc30d5ce515af07d39de955e7a9210cb8ddb53a3
Nacho <nacho_085@hotmail.com>
2022-09-28 17:40:42 +0200

    Interfaz de eliminar agregada

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,6 @@ Route::post('/tareas', [TodosController::class, 'store'])->name('todos');
 Route::get('/tareas/{id}', [TodosController::class, 'show'])->name('todos-edit'); 
 Route::patch('/tareas/{id}', [TodosController::class, 'update'])->name('todos-update'); 
 Route::delete('/tareas/{id}', [TodosController::class, 'destroy'])->name('todos-destroy'); 
+
+Route::resource('categories', CategoriesController::class);
 
